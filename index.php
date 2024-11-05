06f9a039d4a3f4e34ba47ab637c80e390133b272
Ovil8792 <tamuon00@gmail.com>
2024-11-05 08:00:50 +0700

    sửa tên trên cùng vị trí favicon

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sách VN-tổng hợp các loại sách từ tây đến ta</title>
 </head>
 <body>
     
