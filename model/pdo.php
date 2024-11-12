1c788d99e11844faaeb8bf5ea2b5f5d597d2bdc2
Ovil8792 <tamuon00@gmail.com>
2024-11-12 17:17:46 +0700

    chỉnh sửa file pdo và confing đến database

--- model/pdo.php
+++ model/pdo.php
@@ -2,9 +2,9 @@
 /* function kết nối database */
 function pdo_get_connection()
 {
-    $dburl = "mysql:host=localhost;dbname=thucung;charset=utf8";
-    $username = 'root';
-    $password = '';
+    $dburl = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
+    $username = DB_USER;
+    $password = DB_PASS;
 
     $conn = new PDO($dburl, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
