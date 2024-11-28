9632ef9b29b26fd6b7478df97750c6e53ca007fc
Ovil8792 <tamuon00@gmail.com>
2024-11-29 00:05:34 +0700

    sửa lỗi do commit sai cách

---
+++ admin/view/layouts/footer.php
@@ -0,0 +1,90 @@
+<footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2021 &copy; Mazer</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                                href="http://ahmadsaugi.com">A. Saugi</a></p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="./asset/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="./asset/js/bootstrap.bundle.min.js"></script>
+
+    <script src="./asset/vendors/apexcharts/apexcharts.js"></script>
+    <script src="./asset/js/pages/dashboard.js"></script>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Tempus Dominus JS -->
+<script src="https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus/dist/js/tempus-dominus.min.js"></script>
+
+    <script src="./asset/js/main.js"></script>
+    <script src="./asset/vendors/simple-datatables/simple-datatables.js"></script>
+    <script>
+        // Simple Datatable
+        let table1 = document.querySelector('#table1');
+        let dataTable = new simpleDatatables.DataTable(table1);
+
+        let sideDASH =document.getElementById('sad')
+        let sideDAN =document.getElementById('san')
+        let sideDAP =document.getElementById('sap')
+        let sideUSR =document.getElementById('sus')
+        let sideBL =document.getElementById('sbl')
+        let datacheck = document.getElementById('datacheck')
+        
+        if(datacheck){
+            console.log(datacheck);
+        if(datacheck.innerText == 'Following'){
+            sideBL.classList.remove('active')
+            sideDAP.classList.remove('active')
+            sideUSR.classList.remove('active')
+            sideBL.classList.remove('active')
+            sideDASH.classList.add('active')
+            sideDAN.classList.remove('active')
+        }
+        if(datacheck.innerText == 'DataTable'){
+            sideDAN.classList.add('active')
+            sideBL.classList.remove('active')
+            sideUSR.classList.remove('active')
+            sideDASH.classList.remove('active')
+            sideBL.classList.remove('active')
+            sideDAP.classList.remove('active')
+        }
+        if(datacheck.innerText == 'Sản phẩm'){
+            sideDAP.classList.add('active')
+            sideBL.classList.remove('active')
+            sideUSR.classList.remove('active')
+            sideDASH.classList.remove('active')
+            sideBL.classList.remove('active')
+            sideDAN.classList.remove('active')
+        }
+        if(datacheck.innerText == 'Danh sách User'){
+            sideUSR.classList.add('active')
+            sideBL.classList.remove('active')
+            sideDASH.classList.remove('active')
+            sideDAN.classList.remove('active')
+            sideDAP.classList.remove('active')
+        }
+        if(datacheck.innerText == 'Bình luận'){
+            sideBL.classList.add('active')
+            sideUSR.classList.remove('active')
+            sideDASH.classList.remove('active')
+            sideDAN.classList.remove('active')
+            sideDAP.classList.remove('active')
+            sideBL.classList.remove('active')
+        }
+        if(datacheck.innerText== "Quản lý bình luận"){
+            sideBL.classList.add("active")
+            sideDASH.classList.remove('active')
+            sideDAN.classList.remove('active')
+            sideDAP.classList.remove('active')
+            sideUSR.classList.remove('active')
+        }
+    }
+    </script>
+</body>
+
+</html>
